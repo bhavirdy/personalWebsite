9ac6b61a25f7696c8a8ec02f092f65d7f6192bac
Bhavishyan Reddy <102685501+bhavirdy@users.noreply.github.com>
2023-04-29 16:22:25 +0000

    added education section

--- src/Components/Education.jsx
+++ src/Components/Education.jsx
@@ -2,8 +2,15 @@ import React from "react";
 
 const Education = () => {
   return (
-    <section className="light" id="portfolio">
-      <h2>Education</h2>
+    <section className="light" id="education">
+      <h2 style={{ textAlign: "center" }}>Education</h2> <hr />
+      <div className="row">
+        <div className="col-md-6">
+          <h3 style={{ textAlign: "center" }}>University of Cape Town, WC</h3>
+          <h4 style={{ textAlign: "center" }}>BSc Computer Science & Business Computing</h4>
+          <p style={{ textAlign: "center" }}> 2021 - Present</p> <br />
+        </div>
+      </div>
     </section>
   );
 };
